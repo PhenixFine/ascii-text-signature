79d7f6c152c60f8c5e48fcc0cc7585dacb9ec95b
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-08-25 20:45:52 -0400

    Stage 3 of 4 for JetBrains Academy ASCII Text Signature project. • main() - was adjusted according to new requirements. It sets val name to a String array that it receives from bigLetters(), while using getString() to get the name to convert into bigLetters. val status is set to a String it receives from getString(). • bigLetters() - loads the value file into the String array letters. It then uses that to add each latter to the value big String array while going through each character of the name that was passed to bigLetters. Once it completes it re-maps big to adjust the outside spacing of each string, and then returns the array. • printNameTag() - receives a String array of the big letters that form a name, and the status. It checks which one has a longer length and adjusts the padding accordingly. It then prints the big letter name and status with stars surrounding it.

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,51 @@
-fun main() {
-    val name = readLine()!!
+import java.io.File
 
-    println(nameTag(name))
+fun main() {
+    val name = bigLetters(getString("Enter name and surname: "))
+    val status = getString("Enter person's status: ")
+
+    printNameTag(name, status)
 }
 
-fun nameTag(name: String): String {
-    val topBottom = "*".repeat(name.length + 4)
-    val middle = "* $name *\n"
+fun bigLetters(name: String): Array<String> {
+    val file = File("medium.txt")
+    val letters = file.readText().split("\n")
+    val big = Array(3) { "" }
 
-    return topBottom + "\n" + middle + topBottom
+    for (char in name) {
+        if (char != ' ') {
+            val start = letters.indexOfFirst { it.contains("$char ") }
+            for (i in 1..3) big[i - 1] += letters[start + i]
+        } else {
+            for (i in 0..2) big[i] += "     "
+        }
+    }
+    return big.map { "  $it " }.toTypedArray()
+}
+
+fun printNameTag(name: Array<String>, status: String) {
+    val nameL = name[0].length
+    val statL = status.length + 4
+    val bothOdd = nameL % 2 != 0 && statL % 2 != 0
+    val padName = if (nameL >= statL) (name.map { "*$it*" }).toTypedArray() else {
+        (name.map {
+            "*" + " ".repeat((statL / 2) - (if (bothOdd) 0 else nameL % 2) - (nameL / 2)) + it +
+                    " ".repeat((statL / 2) + (if (bothOdd) 0 else statL % 2) - (nameL / 2)) + "*"
+        }).toTypedArray()
+    }
+    val padStatus = if (statL >= nameL) "*  $status  *" else {
+        "*" + " ".repeat(((nameL) / 2) - (if (bothOdd) 0 else statL % 2) - (status.length / 2)) + status +
+                " ".repeat(((nameL) / 2) + (if (bothOdd) 0 else nameL % 2) - (status.length / 2)) + "*"
+    }
+    val topBottom = "*".repeat(padStatus.length)
+
+    println(topBottom)
+    for (string in padName) println(string)
+    println(padStatus)
+    println(topBottom)
+}
+
+fun getString(text: String): String {
+    print(text)
+    return readLine()!!
 }
